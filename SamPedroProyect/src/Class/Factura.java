e2d26a9fa067b9c6c58490f9a175ec3f4175106c
Ivan Mosquera <Ivan Mosquera>
2017-09-20 17:28:34 -0500

    Guardar articulos en arreglo de prendas

--- SamPedroProyect/src/Class/Factura.java
+++ SamPedroProyect/src/Class/Factura.java
@@ -32,11 +32,23 @@ public class Factura {
             ResultSet rs = pst.executeQuery (); //Cargamos datos devueltos de la consulta SELECT en rs
             resul = "Cargado Correctamente";
             System.out.println(resul);
+            while(rs.next()){
+                i++;
+            }
+            prendas = new Prenda[i];
+            i = 0;
+            rs.beforeFirst();
             /*Imprimimos por pantalla todos los idProducto devueltos de la bÃºsqueda*/
             while (rs.next()) {
-                prendas[i] = new Prenda();
-                String productid = rs.getString("id_Producto");
-                System.out.println("Codigo : " + productid);
+                //prendas[i] = new Prenda();
+                int productid = rs.getInt("id_Producto");
+                String detalle = rs.getString("Descripcion");
+                float precio = rs.getFloat("Precio");
+                String talla = rs.getString("Talla");
+                prendas[i] = new Prenda(productid, detalle, talla, precio);
+                System.out.println("Codigo : " + prendas[i].Codigo + " Detalle: " + prendas[i].Detalle + " Precio: " + prendas[i].Precio + " Talla: " + prendas[i].Talla);
+                //System.out.println("Codigo : " + productid + " Detalle: " + detalle + " Precio: " + precio + " Talla: " + talla);
+
                 i++;
             }  
         }catch(SQLException e){
